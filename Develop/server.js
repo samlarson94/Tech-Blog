d25859ea04ac1f1c6061cbee9baefe0d58c9530b
samlarson94 <samlarson94@gmail.com>
2022-05-05 15:54:39 -0500

    Continuing to build out MVC file structure. Adding boiler-plate code.

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3001;
